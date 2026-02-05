beacddb2912431717f237613d908324df4b73279
Samora Machel <samora@me.laptop>
2026-02-05 08:44:12 -0800

    Fixed library importation Cluster not found error

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,8 @@ export { GalaxyGraph } from '../components/GalaxyGraph';
 export { OverlayUI } from '../components/OverlayUI';
 
 // Types
-export type { GraphData, NodeData, LinkData, GraphRef } from '../types';
+export type { GraphData, NodeData, LinkData, GraphRef, ClusterConfig } from '../types';
+export { ClusterId } from '../types';
 
 // Re-export config for consumers who want to use it
 export { default as defaultConfig } from '../data/config.json';
